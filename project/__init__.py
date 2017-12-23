d9c10e3631f7420c6841aba19cb5c900dfe39013
GarthMiles <gmiles@denvergrp.com>
2017-12-23 13:06:43 -0500

    adding db model 'User' class to '__init__' file, creating a 'db' directory in '/project', and adding 'create.sql' to that dir

--- project/__init__.py
+++ project/__init__.py
@@ -16,6 +16,26 @@ app.config.from_object(app_settings)
 # instantiate the db
 db = SQLAlchemy(app)
 
+# db model
+class User(db.Model):
+    """define the table scheme"""
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=False, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, username, email):
+        """initializes and exposes username, email, & created_at 
+        variables to the rest of the class
+        """
+        self.username = username
+        self.email = email 
+        self.created_at = datetime.datetime.utcnow()
+
+
+# app routes
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify({
