3862399f2d81c9b5e1daa92472b42c0835dce5a9
GarthMiles <gmiles@denvergrp.com>
2017-12-23 12:56:31 -0500

    adding 'SQLALCHEMY' instance to 'config' file

--- project/config.py
+++ project/config.py
@@ -5,20 +5,24 @@ class BaseConfig:
     """Base configuration"""
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
-    DEBUG = False 
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
