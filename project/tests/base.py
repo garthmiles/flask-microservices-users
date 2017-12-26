ebc2f47ef32559204737bc74648b8224b25dd4ab
GarthMiles <gmiles@denvergrp.com>
2017-12-26 13:29:35 -0700

    adding test files and defining 'BaseTestCase' class

---
+++ project/tests/base.py
@@ -0,0 +1,18 @@
+# imports - 3rd-party modules
+from flask_testing import TestCase
+# imports - local modules
+from project import app, db 
+
+class BaseTestCase(TestCase):
+    def create_app(self):
+        app.config.from_object('project.config.TestingConfig')
+        return app 
+    
+    def setUp(self):
+        db.create_all()
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        
