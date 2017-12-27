dc271e6d1c272c3c8a059448e92f0d3754dae0b5
GarthMiles <gmiles@denvergrp.com>
2017-12-27 16:01:39 -0700

    created new instance of 'Blueprint' inside 'views' file and bound the 'ping_pong' method to it

---
+++ project/api/views.py
@@ -0,0 +1,13 @@
+# imports - 3rd-party modules
+from flask import Blueprint, jsonify
+
+
+users_blueprint = Blueprint('users', __name__)
+
+
+@users_blueprint.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': "success",
+        'message': "pong!"
+    })
